0a9a3b58bad7e2b5145d99519308d16b07db9633
Ángela Ávila Lasanta <lasantavispa@gmail.com>
2024-02-11 10:28:36 +0100

    Ticket1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/modulo-3-evaluacion-final-Avispas',
   plugins: [react()],
 })
